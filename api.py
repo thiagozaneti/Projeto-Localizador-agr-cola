651f4f1edcce7fef3ecd57e39d9df0c7faae9ba6
ThiagoZaneti <dotinha0203@gmail.com>
2024-05-14 13:54:11 -0300

    banco de dados configurado para finalidade de teste

--- api.py
+++ api.py
@@ -7,20 +7,28 @@ bp = Blueprint("api", __name__)
 def index():
     return render_template("index.html")
 
-# @bp.route("/api/infos", methods = ["GET"])
-# def infos():
-#     if request.method == "GET":
-#         texto = request.args.get("texto")
-#         new_info = Localizacao(texto = texto)
-#         db.session.add(new_info)
-#         db.session.commit()
-#         variavel = ({"message":"texto adicionado com sucesso", "status":200}) 
-#         return render_template("text.html")
-#     else:
-#        return render_template("text.html")
+@bp.route("/api/infos", methods = ["GET"])
+def infos():
+    if request.method == "GET":
+        texto = request.args.get("texto")
+        new_info = Localizacao(texto = texto)
+        db.session.add(new_info)
+        db.session.commit()
+        variavel = ({"message":"texto adicionado com sucesso", "status":200}) 
+        return render_template("text.html")
+    else:
+       return render_template("text.html")
     
-@bp.route("/api/infos/sensores", methods=["GET"])
+@bp.route("/api/infos/sensores", methods=["POST"])
 def receber_sensores():
     data = request.get_json()
+    data_sensor_1 = data.get("sensor1")
+    data_sensor_2 = data.get("sensor2")
+    if request.method == "POST":
+        new_info_um = Localizacao(sensor_um = data_sensor_1)
+        new_info_dois = Localizacao(sensor_um = data_sensor_2)
+        db.session.add(new_info_um, new_info_dois)
+        db.session.commit()
+        return jsonify({"sensor_um":data_sensor_1,"sensor_dois":data_sensor_2, "status":200})
     logging.info(f"Dados recebidos {data}")
     return "dados recebidos com sucesso"
--- models/model.py
+++ models/model.py
@@ -4,4 +4,5 @@ db = SQLAlchemy()
 class Localizacao(db.Model):
     __tablename__ = "infos"
     id = db.Column(db.Integer, primary_key = True)
-    texto = db.Column(db.String(100), nullable=False)   
+    sensor_um = db.Column(db.String(100), nullable=False)
+    sensor_dois = db.Column(db.String(100), nullable=False)   
